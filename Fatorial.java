2ef2afd42b50c05a8197425fb02cef7f8da739fc
tcisio10 <63074263+tcisio10@users.noreply.github.com>
2020-05-30 10:46:20 -0300

    Questão 01
     O que é um Algoritmo Recursivo

---
+++ Fatorial.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+
+public class Fatorial {
+
+  
+    public static int fatorial(int num) {
+
+        
+        if (num <= 1) {
+
+            return 1;
+
+        } else {
+
+          
+            return fatorial(num - 1) * num;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        
+        int numero;
+
+        
+        Scanner entrada = new Scanner(System.in);
+
+        
+        System.out.println("Digite o número que você pretende obter o fatorial.");
+
+       
+        numero = entrada.nextInt();
+
+        
+        System.out.println("O fatorial de " + numero + " é " + fatorial(numero) + ".");
+
+
+    }
+}
