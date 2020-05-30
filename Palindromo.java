5271187966e9c763dd01dba5d6a8708b44746f08
tcisio10 <63074263+tcisio10@users.noreply.github.com>
2020-05-30 11:06:32 -0300

    Create Palindromo.java

---
+++ Palindromo.java
@@ -0,0 +1,33 @@
+
+package javaapplication28;
+
+import java.util.Scanner;
+
+
+public class JavaApplication28 {
+
+  
+    public static void main(String[] args) {
+       
+     Scanner ent = new Scanner(System.in);
+        
+        String contrario = "";
+        
+        System.out.println("Digite uma palavra para verificar se é um palíndromo");
+        String nome = ent.nextLine();
+
+        
+        for (int i = (nome.length() -1); i >= 0; i--) {
+            contrario = contrario + nome.charAt(i);
+        }
+        
+        
+        if (contrario.toLowerCase().equals(nome.toLowerCase())) {
+            System.out.println("\n\n\t\tEssa palavra é palíndromo :)");
+        } else {
+            System.out.println("\n\n\t\tEssa palavra não é palíndromo :(");
+        }
+        
+    }
+    
+}
